1fe6377a4656efa9f9e80b3ab1b41fc9179b5192
HACKERANIL <anilreddy.kotte@optimworks.com>
2024-01-09 19:08:41 +0530

    Review-coments-fixed

---
+++ Cypress/support/servicePage.js
@@ -0,0 +1,14 @@
+const departplace = '[class*="search-results"] tbody tr td';
+const cancleButton = '#exitButton';
+
+Cypress.Commands.add('verifyThatdepartPlaceOnServicePage', (departPlace) => {
+    cy.get(departplace).eq(1).should('be.exist').should('be.visible').should('have.text', departPlace);
+})
+Cypress.Commands.add('verifyThatArrivalPlaceOnServicePage', (arrivalPlace) => {
+    cy.get(departplace).eq(2).should('be.exist').should('be.visible').should('have.text', arrivalPlace);
+})
+Cypress.Commands.add('clickOnCancleButton', () => {
+    cy.get(cancleButton).should('be.exist').should('be.visible').click()
+})
+
+
